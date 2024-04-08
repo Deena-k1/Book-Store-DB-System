7152f8af2c5066830a6361ba2804584cd47a038f
Deena-k1 <151841048+Deena-k1@users.noreply.github.com>
2024-04-08 12:21:09 +0100

    python files added
    just added 2 python files, the 'git.assignment' is open to be manipulated as you please and the tst.py can be used to demonstrate git.ignore

---
+++ git.assignment.py
@@ -0,0 +1,32 @@
+#Created a simple console that takes in member info and writes it to a file
+#please feel free to append, modify, delete as you please :))
+
+# Opening the txt the file in append mode
+
+
+with open('members_info.txt', 'a') as file:
+
+    # Ask each member of the group for their information
+    name = input("Enter your name: ")
+    file.write(f"Name: {name}\n")
+
+    age = input("Enter your age: ")
+    file.write(f"Age: {age}\n")
+
+    hobby = input("Whats your current hobby? ")
+    file.write(f"Current hobby: {hobby}\n")
+
+    food = input("What is your favourte dish? ")
+    file.write(f"Faviourte dish: {food}\n")
+    destination = input("Enter your favorite traveling destination: ")
+    file.write(f"Favorite Traveling Destination: {destination}\n")
+
+    have_pet = input("Do you have a pet? (yes/no) ")
+    if have_pet.lower() == 'yes':
+        pet_name = input("Enter your pet's name: ")
+        file.write(f"Pet Name: {pet_name}\n")
+
+    # Add a newline to separate each member's information
+    file.write('\n')
+
+print("Member information has been stored to members_info.txt.")
---
+++ tst.py
@@ -0,0 +1,2 @@
+
+#created a additional file that can be used for demonstrating gitignore
