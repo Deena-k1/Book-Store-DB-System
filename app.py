c8b92d4f4a95b7cf72739cc4500858c620d3b789
Lara Amusan <larasharon1@aol.co.uk>
2024-04-13 10:33:22 +0100

    Have added the orders function and endpoint code to the db_utils.py and app.py file respectively.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from db_utils import get_all_waitlisted_books
+from db_utils import get_all_waitlisted_books, add_purchase
 
 app = Flask(__name__)
 
@@ -18,7 +18,17 @@ def get_waitlist():
     return jsonify(res)
 
 #purchase a book
-@app.route("/purchase")
+@app.route("/purchase", methods=['POST'])
+def purchase_book():
+    purchase = request.get_json()
+    add_purchase(
+        #date=purchase['date'],
+        #order_id=purchase['order_id'],
+        book_id=purchase['book_id'],
+        customer_name=purchase['customer_name'],
+        delivery=purchase['delivery'],
+    )
+    return purchase
 
 # add customer review
 @app.route("/customerreview")
--- db_utils.py
+++ db_utils.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from config import USER, PASSWORD, HOST
+from datetime import datetime
 
 class DbConnectionError(Exception):
 
@@ -38,6 +39,51 @@ def get_all_waitlisted_books(waitlistbooks):
             if db_connection:
                 db_connection.close()
             
+
+def add_purchase(customer_name, book_id, delivery):
+    try:
+        db_name = 'books?'
+        db_connection = _connect_to_db(db_name)
+        cur = db_connection.cursor()
+        print("Connected to DB: %s" % db_name)
+
+        # generating the current date
+        date = datetime.now().date().strftime('%Y-%m-%d')
+
+        # query to get the current largest order id (should be the id of the latest entry in the table)
+        order_id_query = """
+            SELECT MAX(order_id)
+            FROM orders
+            """
+        cur.execute(order_id_query)
+        current_order_id = cur.fetchone()[0]  # here we are obtaining the result of the above query
+
+        # now we use the above to set the order id of the new order
+        # (we want it to automatically follow the previous order id)
+        if current_order_id is None:
+            order_id = 1
+        else:
+            order_id = current_order_id + 1
+
+        # query to add a new row to the orders table
+        new_order_query = """
+            INSERT INTO orders
+            (date, order_id, book_id, customer_name, delivery)
+            VALUES
+            ('{date}', '{order_id}', '{book_id}', '{customer_name}', '{delivery}')
+        """.format(date=date, order_id=order_id, book_id=book_id, customer_name=customer_name, delivery=delivery)
+        cur.execute(new_order_query)
+
+        db_connection.commit()
+        cur.close()
+
+    except Exception:
+        raise DbConnectionError("Failed to read data from DB")
+
+    finally:
+        if db_connection:
+            db_connection.close()
+            print("DB connection is closed")
 
 
 if __name__ == '__main__':
