b2817942d342b81b49f584faf50b9d8e6e40e037
Deena-k1 <151841048+Deena-k1@users.noreply.github.com>
2024-04-14 13:05:04 +0100

    Update main.py
    added function that interacts with terminal, getting reviews

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 from db_utils import get_available_books
 
-from db_utils import get_all_waitlisted_books
+from db_utils import get_all_waitlisted_books, all_books, reader_review
 
 ### Functions to connect to app endpoints with user input ###
 
@@ -29,6 +29,37 @@ def add_new_order(customer_name, book_id, delivery):
 
 
 ### Run function that interacts with user in terminal ###
+
+#function that will recieve review input from user 
+def handle_review():
+    print("\n" + "ɷ" * 25 + " Review a Book " + "ɷ" * 25)
+    all_books()
+    valid_book_ids = [f'b{i}' for i in range(1, 12)]
+    print("\nPlease select a book to review:")
+    while True:
+        book_id = input("Enter the ID of the book you want to review, e.g., 'b1': ")
+        if book_id in valid_book_ids:
+            break
+        else:
+            print("Invalid book ID. Please enter a valid book ID.")
+
+    customer_name = input("Enter your name: ")
+    while True:
+        ratingstr = input("Enter your rating (1-5): ")
+        try:
+            rating = int(ratingstr)
+            if 1 <= rating <= 5:
+                break
+            else:
+                print("Rating must be between 1 and 5.")
+        except ValueError:
+            print("Please enter a valid number")
+
+    if rating >= 1 and rating <= 5: 
+        reader_review(customer_name, book_id, rating)
+        print("Review added successfully!")  # Display success message
+    else: 
+        print('You have input an invalid option, please select one of the four choices')
 
 
 
@@ -58,7 +89,8 @@ def userOptionSelect(optionSelect):
 
         pass
     elif optionSelect == 'review':
-        # Add review functionality here
+        handle_review()
+       
         pass
     elif optionSelect == 'exit':
         print('Thanks for stopping by!')
